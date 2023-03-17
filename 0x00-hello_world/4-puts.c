d300984c9c73a6bd7b628bbae10aaff3f6cde554
WaithakaMwangi <samuelwaithaka531@gmail.com>
2023-03-17 11:53:20 -0700

    program that prints followed by new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a line put function
+ * return: always 0(success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
